d0a7d880499d3e0ac27b392b0c8f65bec38eadb2
unknown <griel034@gmail.com>
2023-10-27 19:42:48 +0800

    add change

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
         className="fixed top-16 left-0 z-40 w-56 h-screen transition-transform -translate-x-full md:translate-x-0 max-lg:w-16"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 mt-3 overflow-y-auto bg-gray-200 border-gray-300 dark:bg-gray-950">
+        <div className="h-full px-3 py-4 mt-3 overflow-y-auto bg-gray-300 border-gray-300 dark:bg-gray-950">
           <ul className="space-y-10 font-medium">
             {sidebarLinks.map((item) => {
               const route =

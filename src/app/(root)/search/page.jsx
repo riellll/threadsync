b6bfb94692b5753189ff5e531dcfbc111d362973
unknown <griel034@gmail.com>
2023-11-01 11:09:07 +0800

    add change

--- src/app/(root)/search/page.jsx
+++ src/app/(root)/search/page.jsx
@@ -26,7 +26,6 @@ const page = async ({searchParams}) => {
     pageSize: 25,
   });
   
-   console.log(userInfo._id)
   return (
     <section>
       {/* <h1 className="text-3xl font-bold text-black dark:text-gray-200 pb-10">Search</h1> */}
